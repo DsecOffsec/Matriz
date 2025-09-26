b84f01659e4d9a1d817cc31c3fd2e1e30f05dd80
DsecOffsec <dsarabiay@gmail.com>
2025-09-26 02:04:38 -0400

    Update repository.py

--- repository.py
+++ repository.py
@@ -3,6 +3,8 @@ import gspread
 import pandas as pd
 import re
 import json
+import time
+import socket
 from typing import Optional, Iterable
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
@@ -900,6 +902,7 @@ if st.button("Reportar", use_container_width=True):
             st.success(f"Incidente registrado correctamente: {codigo}")
         except Exception as e:
             st.error(f"No se pudo escribir en la hoja: {e}")
+
 
 
 
